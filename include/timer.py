c86e193fc8a9f01847cda2e3364608bbbeb80f3f
thatguywiththehat <mattdraft@gmail.com>
2018-08-31 17:02:48 -0400

    started timer class

---
+++ include/timer.py
@@ -0,0 +1,6 @@
+class timer:
+	endTime = 0
+	def __init__(self, endingTime):
+		endTime = endingTime
+
+		print("end time: " + str(endTime))
---
+++ source/main.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.insert(0, '../include')
+
+from timer import *
+
+
+def main():
+	print("hello world")
+	i = timer(3)
+
+
+
+
+#print("test failed")
+
+if __name__ == '__main__':
+	main()
