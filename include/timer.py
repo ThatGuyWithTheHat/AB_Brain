292486a025fbcfc5d35fa2ab2a48122bab1ee340
thatguywiththehat <mattdraft@gmail.com>
2018-09-01 16:20:42 -0500

    got multi threading working for the timer

--- include/timer.py
+++ include/timer.py
@@ -1,6 +1,30 @@
-class timer:
-	endTime = 0
-	def __init__(self, endingTime):
-		endTime = endingTime
 
-		print("end time: " + str(endTime))
+import time
+class timer:
+	#set up variables
+	endTime = 0 #holds the timer end variable
+	startTime = 0
+	on = False #tells us if the timer is running
+	#set up the timer by passing an end time
+	def __init__(self, endingTime):
+		self.endTime = endingTime * 60
+		print("end time: " + str(self.endTime))
+
+
+
+	def isRunning(self):
+		return self.on
+
+	def timing(self):
+		print(self.endTime)
+		self.on = True #we are running now
+		startTime = time.time()
+		while(1):
+			deltaTime = time.time() - startTime #how much time has ellapsed
+			print(str(deltaTime))
+			if deltaTime >= self.endTime:
+				break
+			time.sleep(0.5)
+		self.on = False #not running
+		print("complete")
+
--- source/main.py
+++ source/main.py
@@ -1,12 +1,36 @@
+#!/bin/env python3.6
+
 import sys
 sys.path.insert(0, '../include')
 
+import threading
+from queue import Queue
+import time
 from timer import *
 
-
+i = timer(.2)
+print_lock = threading.Lock()
+timers = []
 def main():
 	print("hello world")
-	i = timer(3)
+
+	
+	t = threading.Thread(target = beginTimer, args=(0,))
+	t.daemon = True
+	t.start()
+	time.sleep(1)
+	while(i.on):
+		anyActive = False
+		time.sleep(1)
+
+
+def beginTimer(index):
+	print("begin count down")
+	i.timing()
+	timers.append(i)
+
+
+	
 
 
 
